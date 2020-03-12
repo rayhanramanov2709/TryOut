10fbe0a481c361b60cfe71b1f31fcb3d083a0e5e
rayhanramanov2709 <61938502+rayhanramanov2709@users.noreply.github.com>
2020-03-13 04:32:28 +0700

    up

--- js/pertanyaan.js
+++ js/pertanyaan.js
@@ -10,6 +10,15 @@
 				answer5  : "2 < m < 3",
                 correctAnswer: 3
             },
+			 {
+                question : "Grafik fungsi y = (m -3) x2 + 4x - 2m merupakan fungsi definit negatif. Batas-batas nilai m yang memenuhi adalah...",
+                answer1  : "m < 3",
+                answer2  : "m > 3",
+                answer3  : "1 < m < 2",
+				answer4  : "1 < m < 3",
+				answer5  : "2 < m < 3",
+                correctAnswer: 3
+            },
         ],
         points: 0
     };
