68d44bda29e98521a52c420f03e25fe7d64024d7
rayhanramanov2709 <61938502+rayhanramanov2709@users.noreply.github.com>
2020-03-13 06:11:37 +0700

    END

--- js/pertanyaan.js
+++ js/pertanyaan.js
@@ -251,66 +251,7 @@
                 fact:"",
                 correctAnswer: 2
             },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
-			{
-                question : "",
-                answer1  : "",
-                answer2  : "",
-                answer3  : "",
-				answer4  : "",
-				answer5  : "",
-                fact:"Jawaban yang benar adalah ",
-                correctAnswer: 2
-            },
+
         ],
         points: 0
     };
